d742edc24459ba62cf76a99d5a4abe87e7d77a2c
jiangxiujie <jiangxiujie@cantv.cn>
2017-08-18 17:29:16 +0800

    Init a null proj

---
+++ WiFiDetect/app/src/main/java/home_coder/github/com/wifidetect/MainActivity.java
@@ -0,0 +1,13 @@
+package home_coder.github.com.wifidetect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
